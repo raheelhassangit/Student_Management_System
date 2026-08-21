92102c8ce1bcad425a9f6ac0d89644da9dd04c8a
RAHEEL HASSAN <raheelhassanwork@gmail.com>
2026-08-21 14:46:37 +0500

    Attendance integration

--- students/urls.py
+++ students/urls.py
@@ -9,4 +9,5 @@ urlpatterns = [
     path('student/<str:username>/', views.student_profile, name='student_profile'),
     path('student/<str:username>/edit/', views.edit_student, name='edit_student'),
     path('student/<str:username>/delete/', views.delete_student, name='delete_student'),
+    path('attendance/mark/', views.mark_attendance, name='mark_attendance'),
 ]
